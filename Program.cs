6c6f66964fb54846b5f8859a57176686a99b54e2
emanu <emanu@EMANUEL_RAMREZ>
2026-02-28 13:38:45 -0600

    Logica Completa

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿// Mostrar una tabla d
+﻿// Mostrar una tabla de multiplicar del 1 al 5 (todas juntas).
+
+int tabla = 5;
+int multiplo = 5;
+for (int x = 1; x <= tabla; x++)
+{
+    Console.WriteLine($"Tabla de {x} ");
+
+    for (int i = 1; i <= multiplo; i++)
+    {
+        Console.WriteLine($" {x} X {i} = {x * i}");
+    }
+    Console.WriteLine();
+}
